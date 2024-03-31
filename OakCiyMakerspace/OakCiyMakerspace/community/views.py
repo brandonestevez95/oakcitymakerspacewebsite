29074f2a71262a6270d56b81699678e5add9cf5a
Brandon Estevez <73146111+brandonestevez95@users.noreply.github.com>
2024-03-31 13:36:27 -0400

    Update OakCiyMakerspace/OakCiyMakerspace/community/views.py

---
+++ OakCiyMakerspace/OakCiyMakerspace/community/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def discussion_list(request):
+    # View logic for displaying list of discussions
+
+def discussion_detail(request, discussion_id):
+    # View logic for displaying individual discussion
