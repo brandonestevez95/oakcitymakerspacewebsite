061732fab0e5670b2191f87c67e4e483e71b8343
Brandon Estevez <73146111+brandonestevez95@users.noreply.github.com>
2024-04-02 15:54:37 +0000

    edits were made to views.py for blog app in Django

--- OakCiyMakerspace/OakCiyMakerspace/blog/views.py
+++ OakCiyMakerspace/OakCiyMakerspace/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    test = 0 
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, post_id):
-   test = 0 
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
