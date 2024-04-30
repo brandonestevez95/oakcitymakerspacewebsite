3d90e0de5b73a58601dfa76f3e4850d32f0806c0
Brandon Estevez <73146111+brandonestevez95@users.noreply.github.com>
2024-04-30 15:26:44 +0000

    debugged blog models.py file

--- OakCiyMakerspace/OakCiyMakerspace/blog/models.py
+++ OakCiyMakerspace/OakCiyMakerspace/blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -12,3 +13,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
